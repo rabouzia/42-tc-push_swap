0c83d0e08e9581b8897b4dd40e3a8f8d196e4dd3
Ramzy Bouziane <rabouzia@bess-f1r3s14.clusters.42paris.fr>
2024-01-02 15:23:39 +0100

    xavier est un sdf

--- operator.c
+++ operator.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   operator.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ramzerk <rabouzia@student.42.fr>           +#+  +:+       +#+        */
+/*   By: rabouzia <rabouzia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/29 16:26:56 by rabouzia          #+#    #+#             */
-/*   Updated: 2024/01/01 15:01:13 by ramzerk          ###   ########.fr       */
+/*   Updated: 2024/01/02 15:23:23 by rabouzia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,36 +20,6 @@ typedef struct s_node
 	struct s_node	*prev;
 
 }					t_node;
-
-t_node	*ft_lstnew(void *content)
-{
-	t_node	*res;
-
-	res = malloc(sizeof(t_node));
-	if (!res)
-		return (NULL);
-	res->content = content;
-	res->next = NULL;
-	return (res);
-}
-t_node	*ft_lstlast(t_node *lst)
-{
-	if (!lst)
-		return (0);
-	while (lst->next != NULL)
-		lst = lst->next;
-	return (lst);
-}
-
-void	ft_lstadd_back(t_node **lst, t_node *new)
-{
-	if (!lst || !new)
-		return ;
-	if (*lst)
-		ft_lstlast(*lst)->next = new;
-	else
-		*lst = new;
-}
 
 void	ft_lstadd_front(t_node **lst, t_node *new)
 {
@@ -65,13 +35,12 @@ void	swap(t_node *tab)
 	tab->content = tab->next->content;
 	tab->next->content = tmp;
 }
-void	push(t_node *t1, t_node *t2)
+void	push(t_node **t1, t_node **t2)
 {
-	t_node	*tmp;
+	int	tmp;
 
-	tmp = t1->content;
-	t2->content = tmp;
-	delete (t1->content);
+	tmp = pop(t1);
+	append(t2, tmp);
 }
 
 void	rotate(t_node *t1)
@@ -82,3 +51,39 @@ void	rotate(t_node *t1)
 	t1 = t1->next;
 	ft_lstadd_back(t1, tmp);
 }
+
+int	pop(t_node **stack)
+{
+	int		ret;
+	t_node	*current;
+
+	current = *stack;
+	ret = current->content;
+	*stack = current->next;
+	free(current);
+	return (ret);
+}
+
+void delete (t_node **trash)
+{
+}
+
+void	append(t_node **stack, int value)
+{
+	t_node	*new;
+
+	new = malloc(sizeof(t_node));
+	if (!new)
+		return ;
+	new->content = value;
+	new->next = *stack;
+	*stack = new;
+}
+
+/*
+fonction pop qui peut supprimer le premier noeaud de ta liste ;
+et retourner la valeur du noeud en question
+
+puis faire une fonction append qui a partiir d une valeur cree un noeud en debut de chaine pour poiuvoir les creers au debuut et aussi pouvoir les append en milieu
+d execution
+*/
